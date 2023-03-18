d22f32b302c75f062ecb08310d667297d80fa471
Dmitry <chernikdima04@gmail.com>
2023-03-18 18:34:33 +0700

    homework down

--- task4.py
+++ task4.py
@@ -2,10 +2,13 @@ revenue = int(input('Введите выручку фирмы: '))
 costs = int(input('Введите издержки фирмы: '))
 if revenue > costs:
     print(f'Финансовый результат - прибыль. Ее величина: {revenue-costs}')
+    k = round((revenue - costs) / revenue, 1)
+    print(f'Рентабельность выручки = {k}')
+    quantity = int(input('Введите численность сотрудников фирмы: '))
+    profit = round((revenue - costs) / quantity, 1)
+    print(f'Прибыль фирмы в расчете на одного сотрудника = {profit}')
+elif revenue-costs == 0:
+    print('Финансовый результат - фирма вышла в ноль.')
 else:
     print('Финансовый результат - убыль.')
-k = round((revenue-costs)/revenue, 1)
-print(f'Рентабельность выручки = {k}')
-quantity = int(input('Введите численность сотрудников фирмы: '))
-profit = round((revenue-costs)/quantity, 1)
-print(f'Прибыль фирмы в расчете на одного сотрудника = {profit}')
+
